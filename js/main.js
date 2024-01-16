d8aab8d7be618e96b87e887540b428a7a3d13dc7
Paky-Simeone <pasquale.simeone.46@gmail.com>
2024-01-16 19:57:01 +0100

    js-f

--- js/main.js
+++ js/main.js
@@ -1,7 +1,58 @@
 const listaimmagini = ['01.webp', '02.webp', '03.webp','04.webp', '05.webp'];
 const containerimmagini = document.getElementById('container-immagini');
+const arrup = document.getElementById('freccia-su');
+const arrdown = document.getElementById('freccia-giu');
+let indice = 0;
 let slidehtml = ''; 
 
-for(let i = 0; i<listaimmagini.lenght; i++){
-    
+for(let i = 0; i < listaimmagini.length; i++){
+    let immagine = listaimmagini[i];
+    let immagineactive = '';
+
+    if(i == 0){
+        immagineactive = 'active';
+    }
+    else{
+        immagineactive = '';
+    }
+
+   slidehtml += `<img class="immagine ${immagineactive}" src="./img/${immagine}" alt=""></img>` ;
+    console.log(slidehtml);
+
 }
+containerimmagini.innerHTML = slidehtml;
+
+
+arrdown.addEventListener('click', function(){
+
+    let immaginecorrente = document.querySelector('.active');
+    immaginecorrente.classList.remove('active');
+
+    const allslide = document.getElementsByClassName('immagine');
+
+    if(indice < allslide.length -1){
+        indice++;
+    }
+    else{
+        indice = 0;
+    }
+    allslide[indice].classList.add('active');
+})
+
+arrup.addEventListener('click', function(){
+
+    let immaginecorrente = document.querySelector('.active');
+    immaginecorrente.classList.remove('active');
+
+    const allslide = document.getElementsByClassName('immagine');
+
+    if(indice > 0){
+        indice--;
+    }
+    else{
+        indice = 4;
+    }
+    allslide[indice].classList.add('active');
+})
+
+
