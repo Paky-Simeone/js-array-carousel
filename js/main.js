a73bf789da7bbfb03ee63c2679c573cf6cc5f287
Paky-Simeone <pasquale.simeone.46@gmail.com>
2024-01-16 18:32:49 +0100

    js

---
+++ js/main.js
@@ -0,0 +1,7 @@
+const listaimmagini = ['01.webp', '02.webp', '03.webp','04.webp', '05.webp'];
+const containerimmagini = document.getElementById('container-immagini');
+let slidehtml = ''; 
+
+for(let i = 0; i<listaimmagini.lenght; i++){
+    
+}
